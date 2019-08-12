567c2e32ff77155fbb0e8d462887ae6c3b6604d1
sabrinanaumovski <snaumovski@kierantimberlake.com>
2019-08-12 15:33:28 -0400

    -

--- arduino/sketches/ACAW_Side_Stepper_Delta_2/ACAW_Side_Stepper_Delta_2.ino
+++ arduino/sketches/ACAW_Side_Stepper_Delta_2/ACAW_Side_Stepper_Delta_2.ino
@@ -1,19 +1,22 @@
 #include <CapacitiveSensor.h>
 #include <Stepper.h>
 
+#include <AccelStepper.h>
+
 CapacitiveSensor cs_2_4 = CapacitiveSensor(2, 4); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
 unsigned long csSum;
 
 bool lastReading = false;
 bool touching = false;
 bool toggled = false;
-
+bool pos = true;
 
 int readingMin = 2000;
 int delayIncrement = 600;
 int stepIncrement = 60;
 int speedIncrement = 100;
-
+int loopInterval = 50;
+int maxDelay = 600;
 
 // Define number of steps per revolution:
 const int stepsPerRevolution = 200;
@@ -24,27 +27,26 @@ const int brakeA = 9;
 const int brakeB = 8;
 const int dirA = 12;
 const int dirB = 13;
+
 // Initialize the stepper library on the motor shield:
 Stepper myStepper = Stepper(stepsPerRevolution, dirA, dirB);
-unsigned long currentTime;
-unsigned long lastTime;
-unsigned long timeElapsed = 0;
 
-unsigned long loopCycles = 0;
+
+unsigned long timeElapsed = 0;
 
 void setup() {
   cs_2_4.set_CS_AutocaL_Millis(0xFFFFFFFF); // turns off calibration
   Serial.begin(9600);
   
 
-  // Set the PWM and brake pins so that the direction pins can be used to control the motor:
+   //Set the PWM and brake pins so that the direction pins can be used to control the motor:
   pinMode(pwmA, OUTPUT);
   pinMode(pwmB, OUTPUT);
   pinMode(brakeA, OUTPUT);
   pinMode(brakeB, OUTPUT);
 
-  digitalWrite(pwmA, LOW);
-  digitalWrite(pwmB, LOW);
+  digitalWrite(pwmA, HIGH);
+  digitalWrite(pwmB, HIGH);
   digitalWrite(brakeA, LOW);
   digitalWrite(brakeB, LOW);
   
@@ -53,70 +55,56 @@ void setup() {
 }
 
 void loop() {
-  delay(1);
-  timeElapsed += 1;
+  delay(loopInterval);
+  timeElapsed += loopInterval;
+  Serial.println(timeElapsed); 
   long cs = cs_2_4.capacitiveSensor(30);
   lastReading = touching;
 
   // randomize for less 'mechanical' sound = clank... clank, clank! 
-  if(loopCycles >= delayIncrement){
-    // Step quarter revolution in one direction:
-    myStepper.step(stepIncrement);
+  if(timeElapsed >= delayIncrement && toggled){
+    // Step quarter revolution in one direction, switch every other
+    
+    if( pos ){
+      myStepper.step(stepIncrement);
+      pos = false;
+    } else{
+      myStepper.step(-stepIncrement);
+      pos = true;
+    }
+    
     timeElapsed = 0;
+    Serial.println("moved");    
   }
 
- //  delay(random(delayIncrement, delayIncrement + 200));
-
-
-
-
-  //Step quarter revolution in the other direction:
-//  myStepper.step(-stepIncrement);
-//  delay(random(delayIncrement, delayIncrement));
-
-  if (cs > readingMin) {
-    delayIncrement -= 100;
-    //speedIncrement += 1;
+  if (delayIncrement < loopInterval) {
+    delayIncrement = loopInterval;
   }
 
-  if (delayIncrement < 0) {
-    delayIncrement = 0;
-  }
-
-    if (toggled == false) {
-    delayIncrement = 600;
+   if (toggled == false) {
+    delayIncrement = maxDelay;
   }
 
   if (cs > readingMin) {
-    if (touching == false) {
       touching = true;
       Serial.println("touching");
-    }
   } else {
-    if (touching == true) {
       touching = false;
-      Serial.println("not touching");
-    }
   }
 
   if (touching == true && toggled == false && lastReading == false) {
     toggled = true;
-    //speedIncrement = 100;
-    stepIncrement = 60;
-    Serial.println("was toggled, turned on");
+    Serial.println("was off, turned on");
   }
 
   else if (touching == true && toggled == true && lastReading == false) {
     toggled = false;
-    delayIncrement = 600;
-    //speedIncrement = 1;
-    stepIncrement = 0;
-    Serial.println("was toggled, turned off");
+    delayIncrement = maxDelay;
+    Serial.println("was on, turned off");
   }
-  lastTime = millis();
-  Serial.println("lastTime: ");
-  Serial.println(lastTime);
-  Serial.println("delta: ");
-  Serial.println(lastTime-currentTime);
-  Serial.println("--------------------------");
+
+  if(toggled and touching){
+      delayIncrement -= 10;
+  }
+  
 }
